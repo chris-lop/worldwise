f4d2c4d5ddf5cea31f42c43e602d54bccd46d43a
Christopher Lopez <christopherlop12@gmail.com>
2025-09-20 13:52:59 -0400

    fake auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { replace, useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated === true) navigate("/app", { replace: true });
+    },
+    [isAuthenticated, navigate]
+  );
+
   return (
-    <main className={styles.login}>
+    <main className={styles.login} onSubmit={handleSubmit}>
       <PageNav />
       <form className={styles.form}>
         <div className={styles.row}>
